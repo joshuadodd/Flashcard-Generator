f215acc70b7c19ec9eeee25e5ffb431b89cf57fc
joshua dodd <joshuadodd@joshuas-MacBook-Pro.local>
2017-08-02 16:44:23 -0400

    checkpoint

---
+++ BasicCard.js
@@ -0,0 +1,10 @@
+//This file should define a Node module that exports a constructor for creating basic flashcards.
+//The constructor should accept two arguments: `front` and `back`.
+//The constructed object should have a `front` property that contains the text on the front of the card.
+//The constructed object should have a `back` property that contains the text on the back of the card.
+
+module.exports = BasicCard
+
+// Require the 'inquirer' package
+var inquirer = require('inquirer');
+
